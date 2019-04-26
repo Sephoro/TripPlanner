ba3b0250610f6bfbf84cb919f33be51f55889f6c
Mankge <1325914@students.wits.ac.za>
2019-04-26 05:46:41 +0200

    Linking login page with login button

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -10,6 +10,11 @@ router.get('/create', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'account', 'creatingaccount.html'))
 })
 
+router.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'account', 'login.html'))
+})
+
+// Reading user credentials for signing up
 router.post('/api/create', function (req, res) {
   const name = req.body.name
   const surname = req.body.surname
@@ -21,5 +26,30 @@ router.post('/api/create', function (req, res) {
   registeredusers.savenewuser()
   res.redirect('/Home')
 })
+
+// Reading user credentials and checking if they exist on the Database
+// if exists, lead to home page, else reload the login page and try again
+router.post('/api/login', function (req, res) {
+  const email = req.body.email
+  const password = req.body.password
+  Registeredusers.fetchAllusers(userdata => {
+    for (let index = 0; index < userdata.length; index++) {
+      if (email === userdata[index].email && password === userdata[index].password) {
+        res.redirect('/Home')
+        break
+      } else if (email !== userdata[index].email && password === userdata[index].password) {
+        res.redirect('/account/login')
+        break
+      } else if (email === userdata[index].email && password !== userdata[index].password) {
+        res.redirect('/account/login')
+        break
+      } else if (email !== userdata[index].email && password !== userdata[index].password) {
+        res.redirect('/account/login')
+        break
+      }
+    }
+  })
+}
+)
 
 module.exports = router
