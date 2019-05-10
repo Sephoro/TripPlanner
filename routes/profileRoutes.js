69680baeb1caae569f8da2f9d1494cc85b0277dd
Masalane <1408187@students.wits.ac.za>
2019-05-10 20:39:12 +0200

    user is able to reset password

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,6 +2,7 @@
 let path = require('path')
 let express = require('express')
 let router = express.Router()
+let passwordRes = require('../models/ResetPassword')
 let db = require('../data/database')
 
 router.get('/edit', function (req, res) {
@@ -16,20 +17,61 @@ router.get('/profilee', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'profile', 'profilee.html'))
 })
 
+router.get('/PasswordReset', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'profile', 'PasswordReset.html'))
+})
+
 // RESTful interface
 router.get('/api/list', function (req, res) {
   db.pools
     .then((pool) => {
       return pool.request()
 
-        .query("SELECT * FROM users WHERE email = 'Harold.mokoo@gmail.com'")
+        .query("SELECT * FROM users WHERE email = '1408187@students.wits.ac.za'")
     })
     .then(result => {
       res.send(result.recordset)
     })
     .catch(err => {
-      res.send({
-        Error: err
+      res.send({Error: err
+      })
+    })
+})
+
+router.post('/api/RS', function (req, res) {
+  let oldPassword = req.body.password
+  let newPassword = req.body.newPassword
+  let confirmPassword = req.body.confirmPassword
+  let oldPass = false
+  let newPass = false
+
+  db.pools
+    .then((pool) => {
+      return pool.request()
+        .query("SELECT * FROM users WHERE email = '1408187@students.wits.ac.za'")
+    })
+    .then(result => {
+      oldPass = passwordRes.verifyPassword(result.recordset[0], oldPassword)
+
+      if (oldPass === true) {
+        newPass = passwordRes.verifyNewPassword(newPassword, confirmPassword)
+
+        if (newPass === true) {
+          db.pools
+            .then((pool) => {
+              return pool.request()
+                .query("UPDATE users SET password = '" + newPassword + "' WHERE email = '1408187@students.wits.ac.za'")
+            })
+          res.redirect('/')
+        }else {
+          console.log('New password does not match')
+        }
+      }else {
+        console.log('Password didnt match')
+      }
+    })
+    .catch(err => {
+      res.send({Error: err
       })
     })
 })
@@ -42,7 +84,7 @@ router.post('/api/edit', function (req, res) {
   let password = req.body.passWord
   let confirmPassword = req.body.passWord
 
-  let query = 'UPDATE users SET username = \'' + name + '\', surname = \'' + surname + '\', email = \'' + email + '\', cellphone = \'' + cellphone + '\' WHERE email = \'Harold.mokoo@gmail.com\''
+  let query = "UPDATE users SET username = '" + name + "', surname = '" + surname + "', email = '" + email + "', cellphone = '" + cellphone + "' WHERE email = 'Harold.mokoo@gmail.com'"
 
   db.pools
     .then((pool) => {
@@ -55,7 +97,7 @@ router.post('/api/edit', function (req, res) {
 
 router.post('/api/delete', function (req, res) {
   db.pools.then((pool) => {
-    return pool.request().query('DELETE FROM users WHERE email = \'' + req.body.email + '\'')
+    return pool.request().query("DELETE FROM users WHERE email = '" + req.body.email + "'")
   })
 
   res.redirect('/')
