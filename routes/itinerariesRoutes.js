8ccaaa32bd776b69317bd248fb533829243cbdd1
sephoro <eliassepuru@gmail.com>
2019-05-10 22:38:53 +0200

    Itineraries have an itinenary id when saved; Plans are linked to an itinenary

--- routes/itinerariesRoutes.js
+++ routes/itinerariesRoutes.js
@@ -4,60 +4,102 @@ let express = require('express')
 let itineraries = express.Router()
 let db = require('../data/database.js')
 let pf = require('../public/scripts/itineraries/planFunctions')
-let fs = require('fs')
-let counter = require('../data/it.json')
 let session = require('../models/sessions')
 
 itineraries.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'itineraries', 'plan.html'))
 })
 
-itineraries.post('/api/plan', function (req, res) {
-  let duration = pf.durationCalculator(req.body.endDate, req.body.startDate)
-
-  db.pools
-    .then((pool) => {
-      return pool.request()
-
-        .query('INSERT INTO plans (itinerary_id, email,location,activities, duration, startDate, endDate) VALUES (' + counter.counter + ',\'' + 'eliassepuru@gmail.com' + '\',\'' + req.body.location + '\',\'' + req.body.activities + '\',\'' + duration.days + '\',\'' + req.body.startDate + '\',\'' + req.body.endDate + '\')')
-    })
-  res.redirect('/plan')
+itineraries.get('/myplans', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'itineraries', 'myplans.html'))
 })
 
-itineraries.get('/api/myplan', function (req, res) {
+itineraries.post('/api/plan', function (req, res) {
+  let duration = pf.durationCalculator(req.body.endDate, req.body.startDate)
+  let email = session.getUser()
   db.pools
     .then((pool) => {
       return pool.request()
 
-        .query('SELECT * FROM plans WHERE email = \'eliassepuru@gmail.com\' AND itinerary_id = ' + counter.counter + ' ')
+        .query('SELECT * FROM numIt')
     })
     .then(result => {
-      res.send(result.recordset)
+      let itNum = result.recordset[0].counter
+      console.log(email, itNum)
+      db.pools
+        .then((pool) => {
+          return pool.request()
+
+            .query('INSERT INTO plans (itinerary_id, email,location,activities, duration, startDate, endDate) VALUES (' + itNum + ',\'' + email + '\',\'' + req.body.location + '\',\'' + req.body.activities + '\',\'' + duration.days + '\',\'' + req.body.startDate + '\',\'' + req.body.endDate + '\')')
+        })
+      res.redirect('/plan')
     })
-    .catch(err => {
-      res.send({
-        Error: err
-      })
+})
+
+// Return the current plan of the user
+itineraries.get('/api/myplan', function (req, res) {
+  let email = session.getUser()
+
+  db.pools
+    .then((pool) => {
+      return pool.request()
+
+        .query('SELECT * FROM numIt')
+    })
+    .then(result => {
+      let itNum = result.recordset[0].counter
+      db.pools
+        .then((pool) => {
+          return pool.request()
+
+            .query('SELECT * FROM plans WHERE email = \'' + email + '\' AND itinerary_id = ' + itNum + ' ')
+        })
+        .then(results => {
+          res.send(results.recordset)
+        })
+        .catch(err => {
+          res.send({
+            Error: err
+          })
+        })
     })
 })
 
 itineraries.post('/api/save', function (req, res) {
+  // Is the user logged in?
   if (session.loggedIn()) {
+    // Save the iteninary
     db.pools
       .then((pool) => {
         return pool.request()
-          .query('INSERT INTO itineraries (name,email,ItNum) VALUES (\'' + req.body.itName + '\',\'' + session.getUser() + '\',' + counter.counter + ')')
+
+          .query('SELECT * FROM numIt')
+      })
+      .then(result => {
+        let itNum = result.recordset[0].counter
+        db.pools
+          .then((pool) => {
+            return pool.request()
+              .query('INSERT INTO itineraries (name,email,ItNum) VALUES (\'' + req.body.itName + '\',\'' + session.getUser() + '\',' + itNum + ')')
+          })
+      })
+    // Update counter
+    db.pools
+      .then((pool) => {
+        return pool.request()
+
+          .query('SELECT * FROM numIt')
+      })
+      .then(result => {
+        let itNum = result.recordset[0].counter
+        let newIt = itNum + 1
+        db.pools
+          .then((pool) => {
+            return pool.request()
+              .query('UPDATE numIt SET counter = ' + newIt + 'WHERE counter = ' + itNum + ' ')
+          })
       })
 
-    let num = Number(counter.counter)
-    num++
-    counter.counter = num
-    let a = { counter: num }
-    fs.writeFile('data/it.json', JSON.stringify(a), (err) => {
-      if (err) {
-        console.log(err)
-      }
-    })
     res.redirect('/')
   } else {
     res.send('Itineraries can only be saved if you are logged in....Log in if you have an account, sign up or Print the itinary instead by Ctrl+p')
