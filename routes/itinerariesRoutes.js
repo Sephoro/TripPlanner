180d746724a421355b6413fb691e57ca83e214b8
sephoro <eliassepuru@gmail.com>
2019-05-20 11:56:38 +0200

    changes to plans are correctly recorded

--- routes/itinerariesRoutes.js
+++ routes/itinerariesRoutes.js
@@ -5,6 +5,7 @@ let itineraries = express.Router()
 let db = require('../data/database.js')
 let pf = require('../public/scripts/itineraries/planFunctions')
 let session = require('../models/sessions')
+let logMaker = require('../models/planManager')
 
 itineraries.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'itineraries', 'plan.html'))
@@ -235,13 +236,70 @@ itineraries.post('/ourplans/api/edit', function (req, res) {
         .query('UPDATE plans SET location = \'' + req.body.location + '\', activities = \'' + req.body.activities + '\', startDate = \'' + req.body.startDate + '\', endDate = \'' + req.body.endDate + '\', duration = \'' + duration.days + '\' WHERE plan_id = ' + req.body.planid + ' ')
     })
     .then(function () {
-      res.redirect('/plan/ourplans')
+      // res.redirect('/plan/ourplans')
     })
     .catch(err => {
-      res.send({
+      /* res.send({
         Error: err
+      }) */
+      console.log(err)
+    })
+
+  // For generating logs for each plan
+
+  let changes = []
+  let email = session.getUser()
+
+  db.pools
+    .then((data) => {
+      return data.request()
+        .query('SELECT * FROM plans WHERE plan_id = ' + req.body.planid + ' ')
+    })
+    .then(results => {
+      let plan = results.recordset[0]
+      // Check location change
+      if (logMaker.modified(plan.location, req.body.location)) {
+        let modType = logMaker.modificationType(plan.location, req.body.location)
+        changes.push(logMaker.getModification('location', modType, plan.location, req.body.location))
+      }
+      // Check activities changes
+      if (logMaker.modified(plan.activities, req.body.activities)) {
+        let modType = logMaker.modificationType(plan.activities, req.body.activities)
+        changes.push(logMaker.getModification('activities', modType, plan.activities, req.body.activities))
+      }
+      // Check start date changes
+      if (logMaker.modified(plan.startDate, req.body.startDate)) {
+        let modType = logMaker.modificationType(plan.startDate, req.body.startDate)
+        changes.push(logMaker.getModification('startDate', modType, plan.startDate, req.body.startDate))
+      }
+      // Check end date changes
+      if (logMaker.modified(plan.endDate, req.body.endDate)) {
+        let modType = logMaker.modificationType(plan.endDate, req.body.endDate)
+        changes.push(logMaker.getModification('endDate', modType, plan.endDate, req.body.endDate))
+      }
+
+      changes.forEach(change => {
+        // console.log(change)
+        let columns = 'INSERT INTO plans_log (plan_id,contributor, mod_type, mod_date, field, new_field, old_field) '
+        let values = ' VALUES (' + req.body.planid + ',\'' + email + '\',\'' + change.type + '\',\'' + logMaker.getDate() + '\',\'' + change.field + '\',\'' + change.new_ + '\',\'' + change.old + '\') '
+        db.pools
+          .then((data) => {
+            return data.request()
+
+              .query(columns + values) // 'INSERT INTO plans_logs (plan_id,, ItineraryID) VALUES (\'' + session.getUser() + '\',\'' + req.body.email_inivte + '\',\'' + req.body.itinerarieID + '\')')
+          })
+          .catch(err => {
+            console.log(err)
+          })
       })
     })
+    .catch(err => {
+      /* res.send({
+        Error: err
+      }) */
+      console.log(err)
+    })
+  res.redirect('/plan/ourplans')
 })
 
 module.exports = itineraries
