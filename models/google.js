434eaec5fc6111bcbd8c77938afb3b8ed927140b
Mankge <1325914@students.wits.ac.za>
2019-05-24 15:33:36 +0200

    Fixed issue with google ID

--- models/google.js
+++ models/google.js
@@ -10,9 +10,7 @@ module.exports = function (passport) {
     done(null, user)
   })
   passport.use(new GoogleStrategy({
-    // clientID: '206542986789-u442a5ka4qkpghi7gmielr11g40bv3dh.apps.googleusercontent.com',
     clientID: process.env.GOOGLE_CLIENTID,
-    // clientSecret: 'QAYOdGGuKGE7L6mF8zQ0BIWr',
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: '/account/google/redirect'
   }, (token, refreshToken, profile, done) => {
